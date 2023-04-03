169307bfa52551ed23ce36152833ad0ac0529277
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-03 07:02:08 -0400

    Location Manager

--- PlaceLookupDemo/ContentView.swift
+++ PlaceLookupDemo/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var locationManager: LocationManager
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Location:\n\(locationManager.location?.coordinate.latitude ?? 0.0), \(locationManager.location?.coordinate.longitude ?? 0.0)")
         }
         .padding()
     }
@@ -22,5 +22,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(LocationManager())
     }
 }

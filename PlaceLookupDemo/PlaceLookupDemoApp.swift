453186a6bb44b3219b4eff1f25d09b00d727b3ef
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-03 06:24:59 -0400

    Initial Commit

---
+++ PlaceLookupDemo/PlaceLookupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaceLookupDemoApp.swift
+//  PlaceLookupDemo
+//
+//  Created by Lori Rothermel on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaceLookupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
